d875bfc9c34b46a50d4c706531fa8907ee73919a
SunnyGuang <35674943+SunnyGuang@users.noreply.github.com>
2024-08-07 00:56:58 -0400

    Add files via upload

---
+++ build/jackal_description/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sunny/jackal_ws/src/jackal/jackal_description
+# Build directory: /home/sunny/jackal_ws/src/build/jackal/jackal_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(_ctest_jackal_description_roslaunch-check_launch_description.launch "/home/sunny/jackal_ws/src/build/catkin_generated/env_cached.sh" "/usr/bin/python3" "/opt/ros/noetic/share/catkin/cmake/test/run_tests.py" "/home/sunny/jackal_ws/src/build/test_results/jackal_description/roslaunch-check_launch_description.launch.xml" "--return-code" "/usr/bin/cmake -E make_directory /home/sunny/jackal_ws/src/build/test_results/jackal_description" "/opt/ros/noetic/share/roslaunch/cmake/../scripts/roslaunch-check -o \"/home/sunny/jackal_ws/src/build/test_results/jackal_description/roslaunch-check_launch_description.launch.xml\" \"/home/sunny/jackal_ws/src/jackal/jackal_description/launch/description.launch\" ")
+set_tests_properties(_ctest_jackal_description_roslaunch-check_launch_description.launch PROPERTIES  _BACKTRACE_TRIPLES "/opt/ros/noetic/share/catkin/cmake/test/tests.cmake;160;add_test;/opt/ros/noetic/share/roslaunch/cmake/roslaunch-extras.cmake;66;catkin_run_tests_target;/home/sunny/jackal_ws/src/jackal/jackal_description/CMakeLists.txt;8;roslaunch_add_file_check;/home/sunny/jackal_ws/src/jackal/jackal_description/CMakeLists.txt;0;")
