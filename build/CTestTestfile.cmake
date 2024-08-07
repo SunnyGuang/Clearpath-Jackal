331eac6e78742f9d8b59f6809446163a75759ef1
SunnyGuang <35674943+SunnyGuang@users.noreply.github.com>
2024-08-07 00:54:30 -0400

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/sunny/jackal_ws/src
+# Build directory: /home/sunny/jackal_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("jackal/jackal_tutorials")
+subdirs("jackal/jackal_msgs")
+subdirs("jackal/jackal_control")
+subdirs("jackal/jackal_description")
+subdirs("jackal/jackal_navigation")
